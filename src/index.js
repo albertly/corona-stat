ad84b97170bd6acad573d5f9f4fc239df95e0212
albertly <albertly@yandex.ru>
2020-06-22 15:17:45 +0300

    Add snapshot

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { render } from 'react-snapshot';
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 
-ReactDOM.render(
+render(
   <React.StrictMode>
     <CssBaseline />
     <BrowserRouter>
