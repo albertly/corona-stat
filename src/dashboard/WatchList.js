93b69f371217a84635d74c31c44c0bd8c730ec56
albertly <albertly@yandex.ru>
2020-07-01 15:36:57 +0300

    WatchList Search

--- src/dashboard/WatchList.js
+++ src/dashboard/WatchList.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Checkbox from '@material-ui/core/Checkbox';
 import Dialog from '@material-ui/core/Dialog';
@@ -6,20 +7,57 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import TextField from '@material-ui/core/TextField';
+
 import { countryCodes } from '../shared/CountryCodes';
 import { Flag } from '../shared/utils';
+import { red, blue } from '@material-ui/core/colors';
+
+const useStyles = makeStyles((theme) => ({
+
+  list: {
+
+      [theme.breakpoints.down("xl")]: {
+        width: "25vw",
+        height: '60vh',
+        backgroundColor: theme.palette.background.paper,
+        overflow: 'auto',
+      },
+      [theme.breakpoints.down("md")]: {
+        width: "30vw",
+        height: '60vh',
+        backgroundColor: theme.palette.background.paper,
+        overflow: 'auto',
+      },
+      [theme.breakpoints.down("sm")]: {
+        width: "60vw",
+        height: '60vh',
+        backgroundColor:theme.palette.background.paper,
+        overflow: 'auto',
+      }
+
+  },
+
+}));
 
 //ToDo: what is selectedValue ?
 export default function WatchList(props) {
+  const classes = useStyles();
   const { onClose, selectedValue, open } = props;
   const [countries, setCountries] = useState({});
+  const [search, setSearch] = useState('');
 
   const handleClose = () => {
     onClose(selectedValue);
   };
 
+  const handleSearch = e => {
+    setSearch(e.target.value);
+  }
+  
   const descriptionElementRef = useRef(null);
 
   const handleChange = event => {
@@ -55,21 +93,22 @@ export default function WatchList(props) {
   return (
     <>
       <Dialog
-        open={open}
+        open={open}      
         onClose={handleClose}
         scroll="paper"
         aria-labelledby="scroll-dialog-title"
         aria-describedby="scroll-dialog-description"
       >
-        <DialogTitle id="scroll-dialog-title">Subscribe</DialogTitle>
+        <DialogTitle id="scroll-dialog-title">Subscribe</DialogTitle>        
         <DialogContent dividers={true}>
+        <TextField id="search" label="Search" value={search} onChange={handleSearch} />
           <DialogContentText
             id="scroll-dialog-description"
             ref={descriptionElementRef}
             tabIndex={-1}
           >
-
-            {countryCodes.map((e) => (
+            <List className={classes.list} dense component="div" role="list">
+            {countryCodes.filter(el => el.Name.toLowerCase().indexOf(search.toLowerCase()) !== -1).map((e) => (
               <ListItem button onClick={() => { }} key={e.Name}>
 
                 {Flag(e.Name, false)}
@@ -85,7 +124,7 @@ export default function WatchList(props) {
                 />
               </ListItem>
             ))}
-
+          </List>
           </DialogContentText>
         </DialogContent>
         <DialogActions>
