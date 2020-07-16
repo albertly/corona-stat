738538f73fe3d6efac0a536f527c7bcae7326731
albertly <albertly@yandex.ru>
2020-07-16 13:01:05 +0300

    Make automatic changes in service-worker

---
+++ sw-change.js
@@ -0,0 +1,42 @@
+var path = require('path');
+const fs = require('fs');
+
+const file = path.join(__dirname, 'build', 'service-worker.js');
+
+console.log('file', file);
+
+const search = 'workbox.core.clientsClaim();';
+
+const replaceWith = `  
+  self.addEventListener('push', function (evt) {
+    console.log('Push Message Received');
+    var options = {
+      body: 'See What New',
+      data: {
+        timestamp: Date.now(),
+        loc: 'index.html#info',
+      },
+      action: [{ action: 'go', title: 'Go Now' }],
+    };
+    evt.waitUntil(self.registration.showNotification('Corona Stat', options));
+  });
+
+  workbox.core.clientsClaim();
+`;
+
+fs.readFile(file, 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+    throw err;
+  }
+  const newContent = data.replace(search, replaceWith);
+  console.log('data ', newContent);
+
+  fs.writeFile(file, newContent, function (err) {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+    console.log('The file was saved!');
+  });
+});
